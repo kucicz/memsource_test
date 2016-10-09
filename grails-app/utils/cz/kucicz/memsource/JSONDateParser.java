54a9e576bbf158877adbef79052dba2d915b3e61
Vaclav Kucera <vaclav.kucera@wincor-nixdorf.cz>
2016-10-09 21:58:08 +0200

    JSONDateParser cleanup

--- grails-app/utils/cz/kucicz/memsource/JSONDateParser.java
+++ grails-app/utils/cz/kucicz/memsource/JSONDateParser.java
@@ -9,21 +9,14 @@ public class JSONDateParser {
 
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
 
-        //this is zero time so we need to add that TZ indicator for
-        if (input.endsWith("Z")) {
-            input = input.substring(0, input.length() - 1) + "GMT-00:00";
-        } else {
-            int inset = 5;
-
-            String date = input.substring(0, input.length() - inset);
-            String timezone = input.substring(input.length() - inset, input.length());
-            //check timezone format
-            if (!timezone.contains(":")) {
-                timezone = timezone.substring(0, 3) + ":" + timezone.substring(3);
-            }
-            input = date + "GMT" + timezone;
+        int inset = 5;
+        String date = input.substring(0, input.length() - inset);
+        String timezone = input.substring(input.length() - inset, input.length());
+        //check timezone format
+        if (!timezone.contains(":")) {
+            timezone = timezone.substring(0, 3) + ":" + timezone.substring(3);
         }
-
+        input = date + "GMT" + timezone;
         return df.parse(input);
     }
 }
