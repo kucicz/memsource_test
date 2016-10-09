aff9d20fb507fd5f75b3d634363e21e5b183bae1
Vaclav Kucera <vaclav.kucera@wincor-nixdorf.cz>
2016-10-09 21:55:39 +0200

    use stored credentials

--- grails-app/domain/cz/kucicz/memsource/Token.groovy
+++ grails-app/domain/cz/kucicz/memsource/Token.groovy
@@ -4,17 +4,15 @@ class Token {
 
     static constraints = {
     }
+
     String username
-    int passwordHash
     String token
     String expires
-
 
     @Override
     public String toString() {
         return "Token{" +
                 "username='" + username + '\'' +
-                ", passwordHash=" + passwordHash +
                 ", token='" + token + '\'' +
                 ", expires='" + expires + '\'' +
                 '}';
